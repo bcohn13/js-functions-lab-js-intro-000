ac90cc08c5b20f214e7e8ba17c222c6a98dfe5af
Benjamin Cohn <cohnbj13@gmail.com>
2020-03-16 22:21:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+
+function happyCustomHolidayTo(holdiay, name) {
+  return `Happy ${holiday}, ${name}!`
+}
